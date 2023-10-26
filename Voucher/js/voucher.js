7cb38c9a8b3b1e787b9d445ea3a1f0792db190f0
Phạm Quyết Thắng <142492903+phamthang2k4@users.noreply.github.com>
2023-10-26 22:22:02 +0700

    Update voucher.js
    fàdsfà

--- Voucher/js/voucher.js
+++ Voucher/js/voucher.js
@@ -2,8 +2,8 @@ function doclick(tag) {
     if (tag.innerHTML === 'GET') {
         tag.innerHTML = 'GOT';
         tag.style.backgroundColor = "#15a4fa";
-        alert("Chúc mừng bạn đã nhận được Voucher");
+        alert("Congratulations on receiving the voucher!");
     } else if (tag.innerHTML === 'GOT') {
-        alert("Voucher này bạn đã nhận");
+        alert("This voucher you received!");
     }
 }
